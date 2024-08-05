5d1b4458bca186b09265cbd6226a3fb9648af3f9
abhijeet-tezminds <abhijeet@tezminds.com>
2024-08-05 18:33:10 +0530

    added navbar

---
+++ src/app/components/Navbar.jsx
@@ -0,0 +1,26 @@
+import * as React from 'react';
+import Image from "next/image";
+import tezmindsLogo from '../../assets/tezmindslogo.png';
+import profile1 from '../../assets/profile1.png';
+import MenuIcon from '@mui/icons-material/Menu';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import SettingsIcon from '@mui/icons-material/Settings';
+import Avatar from '@mui/material/Avatar';
+
+const Navbar = () => {
+    return (
+        <div style={{width:"100%", display:'flex', justifyContent:"space-between", alignItems:"center"}}>
+            <div style={{display:'flex', alignItems:"center", gap:"2rem"}}>
+                <Image src={tezmindsLogo} alt="Tez Minds Logo"/>
+                <MenuIcon/>
+            </div>
+            <div style={{display:'flex', alignItems:"center", gap:"1rem"}}>
+                <NotificationsIcon/>
+                <Avatar alt="Profile pic" src={profile1.src} />
+                <SettingsIcon />
+            </div>
+        </div>
+    );
+}
+
+export default Navbar;
--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
-import Navbar from "./Navbar.jsx";
+import Navbar from "./components/Navbar.jsx";
+import Sidebar from "./components/Sidebar.jsx"
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <Navbar />
+      <Sidebar />
     </main>
   );
 }
